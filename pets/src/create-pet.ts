390da1c3aac197e527865f70ae88633dba97bd16
Ashel Joseph Vasquez Palomino <ashel.vasquez@alegra.com>
2023-07-05 11:51:19 -0500

    Endpoint para creación de mascotas

--- pets/src/create-pet.ts
+++ pets/src/create-pet.ts
@@ -5,8 +5,9 @@ import {
   APIGatewayProxyResultV2,
 } from 'aws-lambda';
 
-import jwt from 'jsonwebtoken';
 import { ulid } from 'ulid';
+import jwt from 'jsonwebtoken';
+
 import { generateResponse } from '../../utils';
 
 type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
@@ -14,33 +15,30 @@ type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
 const dynamo = new DynamoDB.DocumentClient();
 
 export const handler: ProxyHandler = async (event: APIGatewayProxyEventV2) => {
+	const authorizationToken = event.headers.authorization;
 	const body = JSON.parse(event.body);
 
 	try {
-		const fundationToBeCreated = {
-			fundationId: ulid(),
+		const decodedToken = jwt.verify(authorizationToken, process.env.JWT_SECRET);
+
+		const petToBeCreated = {
+			petId: ulid(),
+			fundationId: decodedToken.fundationId,
 			name: body.name,
+			type: body.type,
+			breed: body.breed,
 		};
 
 		const params = {
-			TableName: process.env.FUNDATIONS_TABLE_NAME || '',
-			Item: {
-				...fundationToBeCreated,
-				accessToken: jwt.sign(
-					{
-						fundationId: fundationToBeCreated.fundationId,
-						fundationName: fundationToBeCreated.name,
-					},
-					process.env.JWT_SECRET
-				),
-			},
+			TableName: process.env.PETS_TABLE_NAME || '',
+			Item: { ...petToBeCreated },
 		};
 
 		await dynamo.put(params).promise();
 
 		return generateResponse({
 			responseBody: {
-				message: 'Fundation Created!',
+				message: 'Pet Created!',
 				data: params.Item,
 			}
 		});
